162662fa2d86ee58ef9a79daed828b80d4518d9d
João Pedro G. C. <115590969+jpgercc@users.noreply.github.com>
2024-07-17 09:41:26 -0300

    form fix

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById('whatsapp-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    let phone = '5551991823420'; // Número de WhatsApp fixo
     let message = document.getElementById('message').value;
 
     // Formatar a mensagem para ser compatível com a URL do WhatsApp
-    let url = `https://wa.me/5551991823420?text=${encodeURIComponent(message)}`;
+    let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
 
     // Abrir o WhatsApp com a mensagem
     window.open(url, '_blank');
